4ab09f28f8099d69f9bb06413ffc7409374fb922
James Huang <39275125+jhu9000@users.noreply.github.com>
2020-09-11 14:36:30 -0700

    Update ucsfpharmacy.drimage.js

--- js/ucsfpharmacy.drimage.js
+++ js/ucsfpharmacy.drimage.js
@@ -7,6 +7,9 @@
     attach: function attach(context) {
       CKEDITOR.on('instanceCreated', function(event) {
         var editor = event.editor;
+        editor.on('dataReady', function() {
+          Drupal.drimage.init(editor.document.$);
+        });
         editor.on('unlockSnapshot', function() {
           Drupal.drimage.init(editor.document.$);
         });
